b09196a8a7f2eb3b2e2282f8f331e02e5578a462
zhangyu <zhangyu48@yusys.com.cn>
2021-05-11 15:16:20 +0800

    sprintV3版本Dto

---
+++ agile-model/src/main/java/com/yusys/agile/sprintV3/dto/SprintV3UserHourDTO.java
@@ -0,0 +1,70 @@
+package com.yusys.agile.sprintV3.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 迭代v3用户小时dto
+ *
+ * @date 2021/05/11
+ */
+@Data
+public class SprintV3UserHourDTO {
+
+    /**
+     * 小时id
+     */
+    private Long hourId;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 迭代id
+     */
+    private Long sprintId;
+
+    /**
+     * 实际工作时长
+     */
+    private Integer reallyHours;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 创建人
+     */
+    private Long createUid;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 更新人
+     */
+    private Long updateUid;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 用户帐户
+     */
+    private String userAccount;
+
+    /**
+     * 租户id
+     */
+    private String tenantCode;
+
+}
