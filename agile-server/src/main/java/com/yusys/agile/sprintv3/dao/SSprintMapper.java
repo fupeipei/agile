72fc25ef70634e13e3f511d1c7b7ada30a2e2694
zhangyu <zhangyu48@yusys.com.cn>
2021-05-27 20:57:57 +0800

    @508562@zhangyu48@迭代概览 删除UserWorkloadQueryModel.java

--- agile-server/src/main/java/com/yusys/agile/sprintv3/dao/SSprintMapper.java
+++ agile-server/src/main/java/com/yusys/agile/sprintv3/dao/SSprintMapper.java
@@ -4,19 +4,11 @@ import com.yusys.agile.sprintV3.dto.SprintListDTO;
 import com.yusys.agile.sprintv3.domain.SSprint;
 import com.yusys.agile.sprintv3.domain.SSprintExample;
 import com.yusys.agile.sprintv3.domain.SSprintWithBLOBs;
-import com.yusys.portal.model.facade.dto.SsoUserDTO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-
-import com.yusys.agile.sprintv3.queryModel.UserWorkloadQueryModel;
-import com.yusys.agile.sprintv3.responseModel.SprintMembersWorkHours;
-import com.yusys.agile.sprintv3.responseModel.SprintOverView;
-import com.yusys.agile.teamv3.domain.STeamMember;
-import com.yusys.portal.model.facade.entity.SsoUser;
-import org.apache.ibatis.annotations.Param;
 
 public interface SSprintMapper {
     long countByExample(SSprintExample example);
