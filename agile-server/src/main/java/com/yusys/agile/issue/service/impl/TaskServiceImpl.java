55b207b15a4f10c69ab9ca415a20bd4dcff5406c
ligg <986486269@qq.com>
2021-05-27 20:22:38 +0800

    @508539@ligg@优化查询系统人员接口

--- agile-server/src/main/java/com/yusys/agile/issue/service/impl/TaskServiceImpl.java
+++ agile-server/src/main/java/com/yusys/agile/issue/service/impl/TaskServiceImpl.java
@@ -259,7 +259,6 @@ public class TaskServiceImpl implements TaskService {
         return issueFactory.queryIssue(taskId, projectId);
     }
 
-    @Override
     @Transactional(rollbackFor = Exception.class)
     public Long createTask(IssueDTO issueDTO) {
         //设置默认创建
@@ -269,14 +268,15 @@ public class TaskServiceImpl implements TaskService {
             stages = new Long[]{StageConstant.FirstStageEnum.READY_STAGE.getValue(), TaskStatusEnum.TYPE_RECEIVED_STATE.CODE};
         } else if (!Optional.ofNullable(stages).isPresent()) {
             stages = new Long[2];
-            List<StageInstance> stageInstances = stageService.getSecondStageListByParentId(StageConstant.FirstStageEnum.READY_STAGE.getValue());
-            stages[0] = StageConstant.FirstStageEnum.READY_STAGE.getValue();
+            stages[0] = StageConstant.FirstStageEnum.DEVELOP_STAGE.getValue();
+            stages[1] = TaskStatusEnum.TYPE_ADD_STATE.CODE;
             //创建用户故事下任务默认放在开发中的就绪阶段、如果关联迭代信息则放在进行中阶段（todo 阶段优化）
+            /*List<StageInstance> stageInstances = stageService.getSecondStageListByParentId(StageConstant.FirstStageEnum.READY_STAGE.getValue());
             if (CollectionUtils.isNotEmpty(stageInstances)) {
                 Long sprintId = issueDTO.getSprintId();
                 StageInstance stageInstance = Optional.ofNullable(sprintId).isPresent() ? stageInstances.get(1) : stageInstances.get(0);
                 stages[1] = stageInstance.getStageId();
-            }
+            }*/
             issueDTO.setStages(stages);
         }
         Long taskId = issueFactory.createIssue(issueDTO, "任务名称已存在！", "新增任务", IssueTypeEnum.TYPE_TASK.CODE);
@@ -520,7 +520,7 @@ public class TaskServiceImpl implements TaskService {
         //根据故事查询所有有效的任务
         List<Issue> tasks = Optional.ofNullable(issueMapper.selectByExample(example)).orElse(new ArrayList<>());
         //完成的数量
-        long finishCount = tasks.stream().filter(t -> t.getStageId().equals(TaskStatusEnum.TYPE_CLOSED_STATE.CODE)).count();
+        long finishCount = tasks.stream().filter(t -> TaskStatusEnum.TYPE_CLOSED_STATE.CODE.equals(t.getStageId())).count();
 
         Issue storyIssue = new Issue();
         storyIssue.setIssueId(storyId);
