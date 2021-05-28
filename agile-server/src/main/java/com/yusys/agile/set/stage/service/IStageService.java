8cac95424d6e413bcfbf57a50a49f796c46f7113
zhaoyd6 <zhaoyd6@yusys.com.cn>
2021-05-28 10:22:13 +0800

    @508637@zhaoyd6@迭代5评审问题修改

--- agile-server/src/main/java/com/yusys/agile/set/stage/service/IStageService.java
+++ agile-server/src/main/java/com/yusys/agile/set/stage/service/IStageService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public interface IStageService {
 
-    List<StageInstance> getStageList();
+    List<StageInstance> getStageList(Integer stageType);
 
     List<StageInstance> getSecondStageListByParentId(Long stageParentId);
 }
