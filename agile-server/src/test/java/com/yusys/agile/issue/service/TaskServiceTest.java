9423c6813a2895fece175b03648ebde3ea685044
dushan1 <dushan1@yusys.com.cn>
2021-05-30 11:09:41 +0800

    @508551@dushan1@任务卡片拖动  增加操作日志

--- agile-server/src/test/java/com/yusys/agile/issue/service/TaskServiceTest.java
+++ agile-server/src/test/java/com/yusys/agile/issue/service/TaskServiceTest.java
@@ -9,10 +9,12 @@ import com.yusys.agile.issue.dto.IssueAttachmentDTO;
 import com.yusys.agile.issue.dto.IssueDTO;
 import com.yusys.agile.issue.dto.StoryCreatePrepInfoDTO;
 import com.yusys.agile.issue.enums.IssueTypeEnum;
+import com.yusys.agile.issue.enums.StoryStatusEnum;
 import com.yusys.agile.issue.enums.TaskStatusEnum;
 import com.yusys.portal.common.exception.BusinessException;
 import com.yusys.portal.model.facade.dto.SecurityDTO;
 import com.yusys.portal.util.thread.UserThreadLocalUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -31,6 +33,7 @@ import java.util.PrimitiveIterator;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {AgileApplication.class})
+@Slf4j
 public class TaskServiceTest {
 
     @Autowired
@@ -116,6 +119,38 @@ public class TaskServiceTest {
         taskService.deleteTask(846446177436880896L, false);
         Assert.assertTrue("deleteTask成功",true);
 
+        SqlLoadTest.execute("classpath:/sql/sqlFileForTaskService.sql",dataSource,resourceLoader);
+        //  task   --119,118,117,116 ,  129  ,128,127,128  ，laneId都是  107
+
+//        TYPE_ADD_STATE("未领取", 107L),
+//        TYPE_RECEIVED_STATE("已领取", 108L),
+//        TYPE_MODIFYING_STATE("进行中", 109L),
+//        TYPE_CLOSED_STATE("已完成", 110L);
+
+
+        //角色id，PO:104，SM:103，TM:105
+        SecurityDTO secDTO = new SecurityDTO();
+        secDTO.setUserId(834451097091657728L);
+        //secDTO.setSystemId(817701268263542784L);
+        secDTO.setTenantCode("1");
+        secDTO.setUserName("杜杉");
+        secDTO.setUserAcct("dushan1");
+        UserThreadLocalUtil.setUserInfo(secDTO);
+        long taskId=119L;
+
+        //故事下的唯一任务被删除，故事状态改为已完成。
+        taskService.deleteTask(126L,false);
+        Issue issue1 = issueMapper.selectByPrimaryKey(219L);
+        Assert.assertEquals("126L故事状态修改", StoryStatusEnum.TYPE_CLOSED_STATE.CODE,issue1.getLaneId());
+        log.info("126L故事状态修改成功");
+
+
+        //故事下的非唯一任务被删除，故事状态不变。
+        taskService.deleteTask(119L,false);
+        issue1 = issueMapper.selectByPrimaryKey(115L);
+        Assert.assertEquals("119L原状态保持不变", StoryStatusEnum.TYPE_MODIFYING_STATE.CODE,issue1.getLaneId());
+        log.info("119L原状态保持不变成功");
+
     }
 
     @Test
@@ -171,6 +206,19 @@ public class TaskServiceTest {
         issue = taskService.dragTask(taskId, 107L, TaskStatusEnum.TYPE_RECEIVED_STATE.CODE, null);
         issue1 = issueMapper.selectByPrimaryKey(taskId);
         Assert.assertEquals("成员拖拽成功",TaskStatusEnum.TYPE_RECEIVED_STATE.CODE+":"+secDTO.getUserId(),issue1.getLaneId()+":"+issue1.getHandler());
+
+        //故事下的非唯一任务被拖动，故事状态不变。
+        taskService.dragTask(119L,107L, TaskStatusEnum.TYPE_CLOSED_STATE.CODE, null);
+        issue1 = issueMapper.selectByPrimaryKey(115L);
+        //进行中
+        Assert.assertEquals("119L原状态保持不变", StoryStatusEnum.TYPE_MODIFYING_STATE.CODE,issue1.getLaneId());
+
+
+
+        //故事下的唯一任务被拖动，故事状态改为已完成。
+        taskService.dragTask(126L,107L, TaskStatusEnum.TYPE_CLOSED_STATE.CODE, null);
+        issue1 = issueMapper.selectByPrimaryKey(219L);
+        Assert.assertEquals("126L故事状态修改", StoryStatusEnum.TYPE_CLOSED_STATE.CODE,issue1.getLaneId());
     }
 
     @Test(expected = BusinessException.class)
@@ -232,8 +280,8 @@ public class TaskServiceTest {
         String userName = "";
         Integer page = 1;
         Integer pageSize = 10;
-        Long systemId = 814801485815332864L;
-        Long storyId = 847060328389558272L;
+        Long systemId = 847156999467307008L;
+        Long storyId = 115L;
         Integer createType = 1;
         StoryCreatePrepInfoDTO taskPreInfo = taskService.getTaskPreInfo(userName, page, pageSize, systemId, storyId, createType);
         System.out.println(taskPreInfo);
