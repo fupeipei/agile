5b477a3af8d356db1186b2ea51560c02f5eabb4c
shenfeng2 <shenfeng2@yusys.com.cn>
2021-05-30 18:03:05 +0800

    @508751@shenfeng2@用户故事修改 agile代码违规修复

--- agile-server/src/main/java/com/yusys/agile/utils/RandomUtils.java
+++ agile-server/src/main/java/com/yusys/agile/utils/RandomUtils.java
@@ -1,14 +1,26 @@
 package com.yusys.agile.utils;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
 
 public class RandomUtils {
+    private static Random rand;
+
+    {
+        try {
+            rand = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+
     public static String randomCode() {
         String code = "";
         Random random = new Random();
         for (int i = 0; i < 6; i++) {
 
-            int a = random.nextInt(10);
+            int a = rand.nextInt(10);
 
             code = code + a;
         }
