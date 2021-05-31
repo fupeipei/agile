7726da080bebea3cbfdd9eff81793299e3a27a07
zhaoyd6 <zhaoyd6@yusys.com.cn>
2021-05-31 15:36:51 +0800

    @508648@zhaoyd6@用户故事批量导入

--- agile-server/src/main/java/com/yusys/agile/easyexcel/service/impl/ExcelServiceImpl.java
+++ agile-server/src/main/java/com/yusys/agile/easyexcel/service/impl/ExcelServiceImpl.java
@@ -49,7 +49,7 @@ public class ExcelServiceImpl implements IExcelService {
     public void uploadStorys(Long systemId, MultipartFile file) throws Exception {
         InputStream inputStream = file.getInputStream();
         //从第一行开始读，待表头
-        List<List<String>> data = ExcelUtil.readExcel(inputStream, 1);
+        List<List<String>> data = ExcelUtil.readExcel(inputStream, 0);
         //校验数据（必填项、数据格式等等）
         checkData(data);
         List<JSONObject> jsonObjects = analysis(data);
