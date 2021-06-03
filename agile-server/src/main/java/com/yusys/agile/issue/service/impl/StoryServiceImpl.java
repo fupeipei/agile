2fe963291023329614863998c8e33d4d4eff96e1
zhaoqj2 <zhaoqj2@yusys.com.cn>
2021-06-03 14:47:51 +0800

    @508529@zhaoqj2@迭代看板展示  查询故事下的Task的查询条件某一个不等于空的情况，判断Children的Task是为空，为空则进行过滤

--- agile-server/src/main/java/com/yusys/agile/issue/service/impl/StoryServiceImpl.java
+++ agile-server/src/main/java/com/yusys/agile/issue/service/impl/StoryServiceImpl.java
@@ -390,11 +390,7 @@ public class StoryServiceImpl implements StoryService {
             //获取故事下的任务
             issueDTOS = getChildren(sprintId, issueDTOS, taskKeyWord, laneIds, taskTypes, handlers);
         }
-        //查询故事下的Task的查询条件某一个不等于空的情况，判断Children的Task是为空，为空则进行过滤
-//        if(StringUtils.isNotBlank(taskKeyWord) || CollectionUtils.isNotEmpty(laneIds)
-//                || CollectionUtils.isNotEmpty(taskTypes) || CollectionUtils.isNotEmpty(handlers)){
-//            storyList = issueDTOS.stream().filter(issue -> CollectionUtils.isEmpty(issue.getChildren())).collect(Collectors.toList());
-//        }
+
         return issueDTOS;
     }
 
@@ -506,6 +502,13 @@ public class StoryServiceImpl implements StoryService {
                     }
                     issueDTO.setChildren(taskList);
                     issueDTOSTmp.add(issueDTO);
+                }else{
+                    //查询故事下的Task的查询条件某一个不等于空的情况，判断Children的Task是为空，为空则进行过滤
+                    if(StringUtils.isNotBlank(taskKeyWord) || CollectionUtils.isNotEmpty(laneIds)
+                            || CollectionUtils.isNotEmpty(taskTypes) || CollectionUtils.isNotEmpty(handlers)){
+                        //storyList = issueDTOS.stream().filter(issue -> CollectionUtils.isEmpty(issue.getChildren())).collect(Collectors.toList());
+                        issueDTOSTmp.add(issueDTO);
+                    }
                 }
 //                if (StringUtils.isEmpty(taskKeyWord) && CollectionUtils.isEmpty(taskList)) {
 //                    issueDTO.setChildren(taskList);
