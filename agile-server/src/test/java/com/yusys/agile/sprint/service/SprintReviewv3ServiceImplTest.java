81642140687ce7670cf60acb7a86a08a700d922e
马雪萍 <maxp2@yusys.com.cn>
2021-06-09 10:43:45 +0800

    #529015#迭代回顾

--- agile-server/src/test/java/com/yusys/agile/sprint/service/SprintReviewv3ServiceImplTest.java
+++ agile-server/src/test/java/com/yusys/agile/sprint/service/SprintReviewv3ServiceImplTest.java
@@ -2,9 +2,7 @@ package com.yusys.agile.sprint.service;
 
 import com.yusys.agile.AgileApplication;
 import com.yusys.agile.sprint.dto.SprintReviewDTO;
-import com.yusys.agile.sprintV3.dto.SprintV3DTO;
 import com.yusys.agile.sprintv3.service.SprintReviewv3Service;
-import com.yusys.portal.common.exception.BusinessException;
 import com.yusys.portal.model.common.dto.ControllerResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
@@ -14,7 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
+import java.util.List;
+
 
 /**
  * @Author maxp2
@@ -45,5 +44,44 @@ public class SprintReviewv3ServiceImplTest {
             Assert.fail("新增迭代回顾信息失败!");
         }
         Assert.assertTrue("新增迭代回顾信成功", true);
+    }
+
+    /**
+     * 查看迭代回顾信息
+     */
+    @Test
+    public void getSprintReviewList() {
+        Long sprintId = 1234L;
+        List<SprintReviewDTO> sprintReviewDTOList = sprintReviewv3Service.getSprintReviewList(sprintId);
+        Assert.assertNotNull(sprintReviewDTOList);
+    }
+
+    /**
+     * 编辑迭代回顾信息
+     */
+    @Test
+    public void editSprintReview() {
+        SprintReviewDTO sprintReviewDTO = new SprintReviewDTO();
+        sprintReviewDTO.setReviewId(852125533103357952L);
+        sprintReviewDTO.setReviewDesc("测试编辑用的");
+        sprintReviewDTO.setReviewType(3L);
+        sprintReviewDTO.setSprintId(1234L);
+        sprintReviewDTO.setTenantCode("1");
+        sprintReviewDTO.setProposeUid(812352455803777024L);
+        int result = sprintReviewv3Service.editSprintReview(sprintReviewDTO);
+        if (result == 0) {
+            Assert.fail("编辑迭代回顾信息失败!");
+        }
+        Assert.assertTrue("编辑迭代回顾信息成功", true);
+    }
+
+    /**
+     * 删除迭代回顾信息
+     */
+    @Test
+    public void deleteSprintReview() {
+        Long reviewId = 852134285109297152L;
+        int i = sprintReviewv3Service.deleteSprintReview(reviewId);
+        Assert.assertTrue("删除迭代回顾信息成功", true);
     }
 }
