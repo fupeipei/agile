a654b9867386bba76ed0005f3611c2d0369ecbb1
libb7 <libb7@yusys.com.cn>
2021-06-10 15:32:23 +0800

    #529010#迭代评审 添加校验

--- agile-server/src/main/java/com/yusys/agile/issue/service/impl/StoryServiceImpl.java
+++ agile-server/src/main/java/com/yusys/agile/issue/service/impl/StoryServiceImpl.java
@@ -998,6 +998,9 @@ public class StoryServiceImpl implements StoryService {
         Issue issue = issueMapper.selectByPrimaryKey(issueDTO.getIssueId());
         List<IssueHistoryRecord> history = new ArrayList<>();
         IssueHistoryRecord nameHistory = null;
+        if(issueDTO.getAssessIsPass()!=1){
+            throw new BusinessException("评审未通过时，需填写评审备注");
+        }
         if (!ObjectUtil.equals(issueDTO.getAssessIsPass(), issue.getAssessIsPass())) {
             nameHistory = IssueHistoryRecordFactory.createHistoryRecord(
                     issue.getIssueId(), IsCustomEnum.FALSE.getValue(), IssueHistoryRecordTypeEnum.TYPE_NORMAL_TEXT.CODE, IssueField.ASSESSISPASS.getDesc());
