4d7d12326c5c51b6f034b7ccda426a9c6ef9733a
ligg <ligg@yusys.com.cn>
2021-07-29 15:04:12 +0800

    #529857#非迭代成员不允许拖动

--- agile-server/src/main/java/com/yusys/agile/issue/service/impl/TaskServiceImpl.java
+++ agile-server/src/main/java/com/yusys/agile/issue/service/impl/TaskServiceImpl.java
@@ -433,16 +433,16 @@ public class TaskServiceImpl implements TaskService {
         if (sprintMembersWorkHours == null || sprintMembersWorkHours.size() == 0) {
             throw new BusinessException("根据迭代标识获取迭代成员信息为空" + task.getSprintId());
         }
+
         //非迭代成员不允许拖拽任务卡片
-        if (Optional.ofNullable(task.getSprintId()).isPresent()){
-            //查询该迭代下的成员
-            List<SprintV3UserHourDTO> sprintUsers = sprintv3Service.getUsersBySprintId(task.getSprintId());
-            //非迭代成员不允许新建任务
-            List<Long> userIds = Optional.ofNullable(sprintUsers).orElse(new ArrayList<>()).stream().map(SprintV3UserHourDTO::getUserId).collect(Collectors.toList());
-            if (CollectionUtils.isNotEmpty(userIds)
-                    && !userIds.contains(UserThreadLocalUtil.getUserInfo().getUserId())){
-                throw new BusinessException("非迭代成员，不允许拖拽任务");
-            }
+        Issue issue = issueMapper.selectByPrimaryKey(task.getParentId());
+        //查询该迭代下的成员
+        List<SprintV3UserHourDTO> sprintUsers = sprintv3Service.getUsersBySprintId(issue.getSprintId());
+        //非迭代成员不允许新建任务
+        List<Long> userIds = Optional.ofNullable(sprintUsers).orElse(new ArrayList<>()).stream().map(SprintV3UserHourDTO::getUserId).collect(Collectors.toList());
+        if (CollectionUtils.isNotEmpty(userIds)
+                && !userIds.contains(UserThreadLocalUtil.getUserInfo().getUserId())){
+            throw new BusinessException("非迭代成员，不允许拖拽任务");
         }
 
         QueryTeamResponse queryTeamResponse = teamv3Service.queryTeam(sprintDTO1.getTeamId());
