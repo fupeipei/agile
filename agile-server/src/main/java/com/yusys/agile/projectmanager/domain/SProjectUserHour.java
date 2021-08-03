3287becad4d7fd64b5ce323a3b7c183a68d2575f
fupp1 <fupp1@yusys.com.cn>
2021-08-03 16:53:02 +0800

    #530055#项目工时#新建

---
+++ agile-server/src/main/java/com/yusys/agile/projectmanager/domain/SProjectUserHour.java
@@ -0,0 +1,37 @@
+package com.yusys.agile.projectmanager.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class SProjectUserHour implements Serializable {
+    private Long hourId;
+
+    private Integer reallyWorkload;
+
+    private Date workDate;
+
+    private Long workUid;
+
+    private Long issueId;
+
+    private Long projectId;
+
+    private String state;
+
+    private Long createUid;
+
+    private Date createTime;
+
+    private Long updateUid;
+
+    private Date updateTime;
+
+    private String tenantCode;
+
+    private String workDesc;
+
+    private static final long serialVersionUID = 1L;
+}
