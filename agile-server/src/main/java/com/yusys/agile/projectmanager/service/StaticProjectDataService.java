e940427589e69737ddd8e3fc23488d40e5e6faec
李艮艮 <ligg@yusys.com.cn>
2021-08-10 14:17:01 +0800

    #530054#删除项目管理的代码

--- agile-server/src/main/java/com/yusys/agile/projectmanager/service/StaticProjectDataService.java
+++
@@ -1,8 +0,0 @@
-package com.yusys.agile.projectmanager.service;
-
-import com.yusys.agile.projectmanager.domain.SStaticProjectData;
-
-public interface StaticProjectDataService {
-
-    SStaticProjectData queryStaticProjectDataById(Long projectStatusId);
-}
